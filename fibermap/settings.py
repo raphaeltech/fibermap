d366df7149c6df74d34493cd21585fdebdee5cb9
raphaeltech <raphael.tech@gmail.com>
2022-03-24 00:13:35 -0300

    Configurando Heroku

--- fibermap/settings.py
+++ fibermap/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "ui", "static", "root")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "ui", "static", "files", "theme"),
@@ -154,7 +154,7 @@ STATICFILES_DIRS = (
 )
 
 # Media files
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "ui", "media")
 
 # User Model
